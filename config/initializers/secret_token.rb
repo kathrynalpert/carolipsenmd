17e8800dde5005d5cb3381e8134f03042909c807
kathrynalpert <kathrynialpert@gmail.com>
2012-12-21 08:16:04 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carolipsenmd::Application.config.secret_token = '2dd172c1451cf1c9df6b82c999cfc70befdf79284f2cb6c584bd7e431818f6243423d07e561514d9845b802fdf4b08bd7701a31d3a5d21174c8523bc49cd597a'
